e7cbbbc573a6e87199191755be41bd8b59e2b6cf
Sofiane Mebchour <sofiane.mebchour@toobi.fr>
2023-03-29 09:40:51 +0200

    Page : - form-signalement.html

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Masquer le bouton d'inscription pour Anonyme et Agent municipal
 const select = document.querySelector('#identification');
 const inscriptionButton = document.querySelector('#inscription-button');
 
@@ -6,5 +7,17 @@ select.addEventListener('change', () => {
     inscriptionButton.classList.add('hidden');
   } else {
     inscriptionButton.classList.remove('hidden');
+  }
+});
+
+// Masquer les cases identifiants et mot de passe pour se connecter
+const selectEl = document.getElementById('identification');
+const fieldsEl = document.getElementById('identification-fields');
+
+selectEl.addEventListener('change', () => {
+  if (selectEl.value === 'habitant' || selectEl.value === 'agent') {
+    fieldsEl.classList.remove('hidden');
+  } else {
+    fieldsEl.classList.add('hidden');
   }
 });
