27bc005f63c3689501580d25bbe6b93b2eb79681
Sofiane Mebchour <sofiane.mebchour@toobi.fr>
2023-03-30 09:45:05 +0200

    Page ajoutée : - Liste signalements

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ const inscriptionButton = document.querySelector('#inscription-button');
 
 select.addEventListener('change', () => {
   if (select.value === 'agent' || select.value === 'anonyme') {
-    inscriptionButton.classList.add('hidden');
+    inscriptionButton.classList.add('d-none');
   } else {
-    inscriptionButton.classList.remove('hidden');
+    inscriptionButton.classList.remove('d-none');
   }
 });
 
@@ -21,3 +21,31 @@ selectEl.addEventListener('change', () => {
     fieldsEl.classList.add('hidden');
   }
 });
+
+// Afficher les signalements en cours
+const signalementsContainer = document.querySelector('#signalements-container');
+
+// Fonction pour récupérer les signalements depuis la base de données
+function getSignalements() {
+  fetch('http://mon-serveur.com/signalements-en-cours')
+    .then(response => response.json())
+    .then(signalements => {
+      // Pour chaque signalement, on crée une carte et on l'ajoute au conteneur
+      signalements.forEach(signalement => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML = `
+          <h2>${signalement.titre}</h2>
+          <p>${signalement.description}</p>
+          <p><strong>Adresse :</strong> ${signalement.adresse}</p>
+          <p><strong>Date :</strong> ${signalement.date}</p>
+          <p><strong>Statut :</strong> ${signalement.statut}</p>
+        `;
+        signalementsContainer.appendChild(card);
+      });
+    });
+}
+
+// Appel de la fonction pour récupérer les signalements au chargement de la page
+getSignalements();
+
